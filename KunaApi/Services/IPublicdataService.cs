97b273bd4c7e79fb240a51ccb7dede248b689f0f
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-08 23:48:56 +0300

    Create PublicdataService

---
+++ KunaApi/Services/IPublicdataService.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using KunaApi.DTO.Answers;
+
+namespace KunaApi.Services
+{
+    public interface IPublicdataService
+    {
+        Task<Timestamp> GetTimestampAsync();
+    }
+}
---
+++ KunaApi/Services/Implements/PublicdataService.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using KunaApi.DTO.Answers;
+using KunaApi.POCO.Requests;
+using KunaApi.POCO;
+
+namespace KunaApi.Services.Implements
+{
+    public class PublicdataService : KunaHttp, IPublicdataService
+    {
+        public PublicdataService() : base() { }
+
+        public async Task<Timestamp> GetTimestampAsync()
+            => await HttpGetAsync<Timestamp>(new TimestampRequest());
+    }
+}
