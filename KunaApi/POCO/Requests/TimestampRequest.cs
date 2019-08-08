4c61afd08bc65ae081b710ef96e506daebe66450
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-08 23:48:37 +0300

    Create TimestampRequest.cs

---
+++ KunaApi/POCO/Requests/TimestampRequest.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaApi.POCO.Requests
+{
+    public class TimestampRequest : KunaRequest
+    {
+        public TimestampRequest() : base()
+            => sb.Append("/timestamp");
+    }
+}
