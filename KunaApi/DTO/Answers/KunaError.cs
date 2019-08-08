d29e083b86a1b88c8b1c830d8cadec8785d66ea1
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-08 23:00:00 +0300

    Create KunaError.cs

---
+++ KunaApi/DTO/Answers/KunaError.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class KunaError
+    {
+        [JsonProperty("error")]
+        public Error Error { get; set; }
+    }
+
+    public class Error
+    {
+        [JsonProperty("code")]
+        public ushort ErrorCode { get; set; }
+
+        [JsonProperty("message")]
+        public string ErrorMessage { get; set; }
+    }
+}
