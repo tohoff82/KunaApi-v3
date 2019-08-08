c43b73054da9e051851d861e10f8b94a716fd9f4
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-08 23:49:53 +0300

    Update KunaApi.cs
    Add PublicdataService to DI cotainer

--- KunaApi/KunaApi.cs
+++ KunaApi/KunaApi.cs
@@ -1,8 +1,22 @@
-﻿using System;
+﻿using KunaApi.Services;
+using KunaApi.Services.Implements;
+using Microsoft.Extensions.DependencyInjection;
+using System;
 
 namespace KunaApi
 {
     public class KunaApi
     {
+        private readonly IServiceProvider provider;
+
+        public KunaApi()
+        {
+            provider = new ServiceCollection()
+                .AddTransient<IPublicdataService, PublicdataService>()
+                .BuildServiceProvider();
+        }
+
+        public IPublicdataService Publicdata
+            => provider.GetService<IPublicdataService>();
     }
 }
