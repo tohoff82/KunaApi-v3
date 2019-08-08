67b084efa1b935863d027074beabb2126cf16ced
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-08 22:05:53 +0300

    Create Timestamp.cs

---
+++ KunaApi/DTO/Answers/Timestamp.cs
@@ -0,0 +1,13 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class Timestamp
+    {
+        [JsonProperty("timestamp")]
+        long InSeconds { get; set; }
+
+        [JsonProperty("timestamp_miliseconds")]
+        long InMiliseconds { get; set; }
+    }
+}
