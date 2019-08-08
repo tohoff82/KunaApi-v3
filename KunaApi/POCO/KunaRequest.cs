20c4b8639e1787f9dcb9c887b2c38723bfc7539b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-08 22:59:53 +0300

    Create KunaRequest.cs

---
+++ KunaApi/POCO/KunaRequest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KunaApi.POCO
+{
+    public class KunaRequest
+    {
+        private readonly string vers = "/v3";
+        protected StringBuilder sb;
+
+        public KunaRequest()
+            => sb = new StringBuilder(vers);
+
+        public Uri Uri
+            => new Uri(sb.ToString(), UriKind.Relative);
+    }
+}
