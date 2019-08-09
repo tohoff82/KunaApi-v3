14e14da26f5e067c4f2ca832f8230d1abddb9675
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-09 07:37:08 +0300

    Create RateRequest.cs

---
+++ KunaApi/POCO/Requests/RateRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class RateRequest : KunaRequest
+    {
+        public RateRequest() : base()
+            => sb.Append("/exchange-rates");
+
+        public RateRequest(string currencyName) : this()
+            => sb.Append(currencyName.ToLower());
+    }
+}
