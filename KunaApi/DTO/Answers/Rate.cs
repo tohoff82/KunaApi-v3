749ca7a1061809e6f8a7323b1f13e960e0ce5a45
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-09 07:37:01 +0300

    Create Rate.cs

---
+++ KunaApi/DTO/Answers/Rate.cs
@@ -0,0 +1,25 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class Rate
+    {
+        [JsonProperty("currency")]
+        public string BaseCurrency { get; set; }
+
+        [JsonProperty("usd")]
+        public float QuotedUSD { get; set; }
+
+        [JsonProperty("uah")]
+        public float QuotedUAH { get; set; }
+
+        [JsonProperty("btc")]
+        public float QuotedBTC { get; set; }
+
+        [JsonProperty("eur")]
+        public float QuotedEUR { get; set; }
+
+        [JsonProperty("rub")]
+        public float QuotedRUB { get; set; }
+    }
+}
