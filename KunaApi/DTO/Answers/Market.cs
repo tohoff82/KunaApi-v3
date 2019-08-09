ae2e163425699703c24b106b4ba9efbbe6f46dbb
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-09 08:39:39 +0300

    Create Market.cs

---
+++ KunaApi/DTO/Answers/Market.cs
@@ -0,0 +1,28 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class Market
+    {
+        [JsonProperty("id")]
+        public string MarketMarker { get; set; }
+
+        [JsonProperty("base_unit")]
+        public string BaseUnitMarker { get; set; }
+
+        [JsonProperty("quote_unit")]
+        public string QuoteUnitMarker { get; set; }
+
+        [JsonProperty("base_precision")]
+        public byte BasePrecision { get; set; }
+
+        [JsonProperty("quote_precision")]
+        public byte QuotePrecision { get; set; }
+
+        [JsonProperty("display_precision")]
+        public byte DisplayPrecision { get; set; }
+
+        [JsonProperty("price_change")]
+        public float PriceChange { get; set; }
+    }
+}
