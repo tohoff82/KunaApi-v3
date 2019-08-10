0437d7a6f71ae7c7279588f99921dc11226acc82
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-10 09:34:54 +0300

    Create TickerRequest.cs

---
+++ KunaApi/POCO/Requests/TickerRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class TickerRequest : KunaRequest
+    {
+        private TickerRequest() : base()
+            => sb.Append("/tickers");
+
+        public TickerRequest(string marketMarker) : this()
+            => sb.AppendFormat("?symbols={0}", marketMarker);
+    }
+}
