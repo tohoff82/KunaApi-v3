f5e638f9ed4dbf1fbedeeb2de305297a12079aff
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-10 15:29:41 +0300

    Create OrderbookRequest.cs

---
+++ KunaApi/POCO/Requests/OrderbookRequest.cs
@@ -0,0 +1,8 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class OrderbookRequest : KunaRequest
+    {
+        public OrderbookRequest(string marketMarker) : base()
+            => sb.AppendFormat("/book/{0}", marketMarker);
+    }
+}
