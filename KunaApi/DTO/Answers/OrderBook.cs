21ba2efcdba98948569f56693c25242127a7e6c6
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-10 15:29:56 +0300

    Create OrderBook.cs

---
+++ KunaApi/DTO/Answers/OrderBook.cs
@@ -0,0 +1,27 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace KunaApi.DTO.Answers
+{
+    [JsonArray]
+    public class Orderbook
+    {
+        [JsonProperty("bid")]
+        public ICollection<OrderbookItem> BidCollection { get; set; }
+
+        [JsonProperty("ask")]
+        public ICollection<OrderbookItem> AskCollection { get; set; }
+    }
+
+    public class OrderbookItem
+    {
+        [JsonProperty("price")]
+        public float Price { get; set; }
+
+        [JsonProperty("volume")]
+        public float Volume { get; set; }
+
+        [JsonProperty("quantity")]
+        public short Quantity { get; set; }
+    }
+}
