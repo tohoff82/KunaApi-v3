35cc419fe27e344037d3753148333ba00fc0bb4a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 07:14:22 +0300

    Create AccountRequest.cs

---
+++ KunaApi/POCO/Requests/AccountRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class AccountRequest : KunaRequest
+    {
+        public AccountRequest() : base()
+        {
+            sb.Append("/auth/me");
+            crudeBody = new object();
+        }
+    }
+}
