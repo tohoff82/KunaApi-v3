fd83230f23f182325e836296290471d536401f9d
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 07:17:08 +0300

    Update KunaRequest.cs

--- KunaApi/POCO/KunaRequest.cs
+++ KunaApi/POCO/KunaRequest.cs
@@ -1,18 +1,28 @@
 ﻿using System;
 using System.Text;
-using System.Collections.Generic;
+using Newtonsoft.Json;
 
 namespace KunaApi.POCO
 {
     public class KunaRequest
     {
         private readonly string vers = "/v3";
+
         protected StringBuilder sb;
+        protected object crudeBody;
 
         public KunaRequest()
             => sb = new StringBuilder(vers);
 
         public Uri Uri
             => new Uri(sb.ToString(), UriKind.Relative);
+
+        public string Body
+            => JsonConvert.SerializeObject(crudeBody);
+
+        public string Signature(string nonce)
+            => new StringBuilder(Uri.ToString())
+            .AppendFormat("{0}{1}", nonce, Body)
+            .ToString();
     }
 }
