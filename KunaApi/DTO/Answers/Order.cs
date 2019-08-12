75879d9ed00ac5033980994e306a45a31f2b9dbb
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 18:37:02 +0300

    Create Order.cs

---
+++ KunaApi/DTO/Answers/Order.cs
@@ -0,0 +1,40 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class Order
+    {
+        [JsonProperty("id")]
+        public long Id { get; set; }
+
+        [JsonProperty("market_marker")]
+        public string MarketMarker { get; set; }
+
+        [JsonProperty("creation_time")]
+        public long CreationTime { get; set; }
+
+        [JsonProperty("update_time")]
+        public long UpdateTime { get; set; }
+
+        [JsonProperty("volume")]
+        public float Volume { get; set; }
+
+        [JsonProperty("start_volume")]
+        public float StartVolume { get; set; }
+
+        [JsonProperty("type")]
+        public string Type { get; set; } // LIMIT or MARKET
+
+        [JsonProperty("status")]
+        public string Status { get; set; } // ACTIVE, EXECUTED
+
+        [JsonProperty("side")]
+        public string Side { get; set; } // BUY, SELL
+
+        [JsonProperty("price")]
+        public float Price { get; set; }
+
+        [JsonProperty("average_price")]
+        public float AveragePrice { get; set; }
+    }
+}
