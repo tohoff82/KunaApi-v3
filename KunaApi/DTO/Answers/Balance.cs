8e1339116e8eb6af656006b1a5fb494c2970c5ed
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 07:13:45 +0300

    Create Balance.cs

---
+++ KunaApi/DTO/Answers/Balance.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class Balance
+    {
+        [JsonProperty("balance_marker")]
+        public string BalanceMarker { get; set; }
+
+        [JsonProperty("currency_marker")]
+        public string CurrencyMarker { get; set; }
+
+        [JsonProperty("full_balance")]
+        public float FullBalace { get; set; }
+
+        [JsonProperty("available_funds")]
+        public float AvailableFunds { get; set; }
+    }
+}
