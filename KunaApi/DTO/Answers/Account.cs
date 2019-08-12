d0dcdc83f24822d2d8cc2e6fd7a6516b252cd3d2
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 07:13:41 +0300

    Create Account.cs

---
+++ KunaApi/DTO/Answers/Account.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Answers
+{
+    public class Account
+    {
+        [JsonProperty("email")]
+        public string Email { get; set; }
+
+        [JsonProperty("kunaid")]
+        public string KunaId { get; set; }
+
+        [JsonProperty("two_factor")]
+        public bool EnableTwoFactor { get; set; }
+
+        [JsonProperty("withdraw_confirmation")]
+        public bool WithdrawConfirmation { get; set; }
+
+        [JsonProperty("public_keys")]
+        public InputOutputKeys InputOutputKeys { get; set; }
+
+        [JsonProperty("announcements")]
+        public bool Announcements { get; set; }
+    }
+
+    public class InputOutputKeys
+    {
+        [JsonProperty("deposit_sdk_uah_public_key")]
+        public string PublicKeyUAH { get; set; }
+
+        [JsonProperty("deposit_sdk_usd_public_key")]
+        public string PublicKeyUSD { get; set; }
+
+        [JsonProperty("deposit_sdk_rub_public_key")]
+        public string PublicKeyRUB { get; set; }
+    }
+}
