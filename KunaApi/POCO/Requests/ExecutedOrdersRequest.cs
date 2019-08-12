8e41e13bf25576e900ebd4c3c14617893d26d63b
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 20:23:29 +0300

    Create ExecutedOrdersRequest.cs

---
+++ KunaApi/POCO/Requests/ExecutedOrdersRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class ExecutedOrderRequest : KunaRequest
+    {
+        public ExecutedOrderRequest(string marketMarker) : base()
+        {
+            _path.AppendFormat("/auth/r/orders/{0}/hist", marketMarker);
+            _requestBody = new object();
+        }
+    }
+}
