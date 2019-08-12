b1c2707939581742da7d73d722410819484f3703
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 18:48:51 +0300

    Create ActiveOrdersRequest.cs

---
+++ KunaApi/POCO/Requests/ActiveOrdersRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class ActiveOrdersRequest : KunaRequest
+    {
+        public ActiveOrdersRequest(string marketMarker) : base()
+        {
+            _path.AppendFormat("/auth/r/orders/?{0}", marketMarker);
+            _requestBody = new object();
+        }
+    }
+}
