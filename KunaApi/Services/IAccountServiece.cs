d2cb13d41e670d6f9d4beb439e120b3b556639aa
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 07:37:29 +0300

    Add two methods
    1. GetAccountAsync
    2. GetBalancesAsync

--- KunaApi/Services/IAccountServiece.cs
+++ KunaApi/Services/IAccountServiece.cs
@@ -1,10 +1,14 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
+using KunaApi.DTO.Answers;
 
 namespace KunaApi.Services
 {
     public interface IAccountServiece
     {
+        Task<Account> GetAccountAsync();
+        Task<IEnumerable<Balance>> GetBalanceAsync();
     }
 }
--- KunaApi/Services/Implements/AccountService.cs
+++ KunaApi/Services/Implements/AccountService.cs
@@ -1,10 +1,29 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using KunaApi.DTO.Answers;
+using KunaApi.POCO.Requests;
+using KunaApi.POCO;
 
 namespace KunaApi.Services.Implements
 {
-    public class AccountService : IAccountServiece
+    public class AccountService : KunaHttp, IAccountServiece
     {
+        private readonly IAuthconfigService _authconfig;
+        private readonly IModelbuilderService _modelbuilder;
+
+        public AccountService(IAuthconfigService authconfig, IModelbuilderService modelbuilder) : base()
+        {
+            _authconfig = authconfig;
+            _modelbuilder = modelbuilder;
+        }
+
+        public async Task<Account> GetAccountAsync()
+            => await HttpPostAsync<Account>(new AccountRequest(), _authconfig.PublicKey, _authconfig.SecretKey);
+
+        public async Task<IEnumerable<Balance>> GetBalanceAsync()
+        {
+            string[][] crudeBalances = await HttpPostAsync<string[][]>(new BalanceRequest(), _authconfig.PublicKey, _authconfig.SecretKey);
+            return _modelbuilder.CreateBalances(crudeBalances);
+        }
     }
 }
