9d181bab413c0e52ac4296fee722968375ec2dfd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-12 07:14:27 +0300

    Create BalanceRequest.cs

---
+++ KunaApi/POCO/Requests/BalanceRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace KunaApi.POCO.Requests
+{
+    public class BalanceRequest : KunaRequest
+    {
+        public BalanceRequest() : base()
+        {
+            sb.Append("/auth/r/wallets");
+            crudeBody = new object();
+        }
+    }
+}
