4cc276c237a4b655a2ac7d167ec0c08049ae3c13
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-13 07:05:48 +0300

    Create Payments.cs

---
+++ KunaApi/DTO/Additions/Payments.cs
@@ -0,0 +1,12 @@
+﻿namespace KunaApi.DTO.Additions
+{
+    public static class Payments
+    {
+        public static string ADVCash = "advcash_wallet";
+        public static string Payeer = "payeer";
+        public static string PerfectMoneyAccount = "perfectmoney_account";
+        public static string PerfectMoneyTransfer = "perfectmoney_transfer";
+        public static string Kunacode = "kuna_code";
+        public static string PaymentCard = "payment_card";
+    }
+}
