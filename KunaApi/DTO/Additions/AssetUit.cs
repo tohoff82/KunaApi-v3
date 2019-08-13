94e668e24b2de3c8ebf7e6e1171b205e305332ca
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-13 07:05:44 +0300

    Create AssetUit.cs

---
+++ KunaApi/DTO/Additions/AssetUit.cs
@@ -0,0 +1,47 @@
+﻿namespace KunaApi.DTO.Additions
+{
+    public static class AssetUit
+    {
+        public static string Bitcoin  = "BTC";
+        public static string Ethereum = "ETH";
+        public static string Dash     = "DASH";
+        public static string Litecoi  = "LTC";
+        public static string Ripple   = "XRP";
+        public static string Stellar  = "XLM";
+        public static string EOS      = "EOS";
+
+        public static string DreamTeam = "DREAM";
+
+        public static string EthereumClassic = "ETC";
+
+        public static string BitcoinCash    = "BCH";
+        public static string BitcoinCashSV  = "BSV";
+        public static string BitcoinCashABC = "BAB";
+
+        public static string Waves = "WAVES";
+        public static string Golos = "GOL";
+
+        public static string Remme = "REM";
+        public static string Karbo = "KRB";
+        public static string Nem   = "XEM";
+
+        public static string B2bx   = "B2B";
+        public static string Hacken = "HKN";
+        public static string ZCash  = "ZEC";
+
+        public static string AdvancedUSD   = "AUSD";
+        public static string AdvancedRUB   = "ARUB";
+        public static string TrueUSD       = "TUSD";
+        public static string Tether        = "USDT";
+        public static string StasisEuro    = "EURS";
+        public static string PaytomatToken = "PTI";
+
+        public static string KunaToken = "KUN";
+        public static string BinanceCoin = "BNB";
+
+        public static string UkrainianHryvnia = "UAH";
+        public static string USDollar         = "USD";
+        public static string RussianRuble     = "RUB";
+        public static string Euro             = "EUR";
+    }
+}
