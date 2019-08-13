22bb268c9e2f63c8689c9d27b0be46df2bcf255a
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-13 07:33:22 +0300

    Create Markets.cs

---
+++ KunaApi/DTO/Additions/Markets.cs
@@ -0,0 +1,51 @@
+﻿namespace KunaApi.DTO.Additions
+{
+    public static class Markets
+    {
+        public static string Btc_Uah   = "btcuah";
+        public static string Bnb_Uah   = "bnbuah";
+        public static string Eth_Uah   = "ethuah";
+        public static string Dash_Uah  = "dashuah";
+        public static string Xrp_Uah   = "xrpuah";
+        public static string Ltc_Uah   = "ltcuah";
+        public static string Eos_Uah   = "eosuah";
+        public static string Dream_Uah = "dreamuah";
+        public static string Krb_Uah   = "krbuah";
+        public static string Xem_Uah   = "xemuah";
+        public static string Rem_Uah   = "remuah";
+        public static string Gol_Uah   = "goluah";
+        public static string Waves_Uah = "wavesuah";
+        public static string Zec_Uah   = "zecuah";
+        public static string Bch_Uah   = "bchuah";
+        public static string Pti_Uah   = "ptiuah";
+        public static string Xlm_Uah   = "xlmuah";
+        public static string UsdT_Uah  = "usdtuah";
+        public static string Usd_Uah   = "usduah";
+        public static string TUsd_Uah  = "tusduah";
+        public static string Eurs_Uah  = "eursuah";
+        public static string Btc_UsdT  = "btcusdt";
+        public static string Eth_UsdT  = "ethusdt";
+        public static string Bnb_UsdT  = "bnbusdt";
+        public static string Gbg_UsdT  = "gbgusdt";
+        public static string Kun_Btc   = "kunbtc";
+        public static string Bnb_Btc   = "bnbbtc";
+        public static string Eth_Btc   = "ethbtc";
+        public static string Eos_Btc   = "eosbtc";
+        public static string Bch_Btc   = "bchbtc";
+        public static string Dream_Btc = "dreambtc";
+        public static string Hkn_Btc   = "hknbtc";
+        public static string Arn_Btc   = "arnbtc";
+        public static string Pti_Btc   = "ptibtc";
+        public static string Gol_Btc   = "golbtc";
+        public static string Btc_Eurs  = "btceurs";
+        public static string Eth_Usd   = "ethusd";
+        public static string Btc_Usd   = "btcusd";
+        public static string Bnb_Usd   = "bnbusd";
+        public static string Rem_Eth   = "remeth";
+        public static string Btc_Rub   = "btcrub";
+        public static string Bnb_Rub   = "bnbrub";
+        public static string Eth_Rub   = "ethrub";
+        public static string Xrp_Rub   = "xrprub";
+        public static string UsdT_Rub  = "usdtrub";
+    }
+}
