fa65941d0bd217dd9af3c4e597d44571b5e869fd
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-08-13 07:13:04 +0300

    Create OrderStatus.cs

---
+++ KunaApi/DTO/Additions/OrderStatus.cs
@@ -0,0 +1,10 @@
+﻿namespace KunaApi.DTO.Additions
+{
+    public static class OrderStatus
+    {
+        public static string Active   = "ACTIVE";
+        public static string Executed = "EXECUTED";
+        public static string Canceled = "CANCELED";
+
+    }
+}
