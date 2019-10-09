38c5ad87ac5b959d3e3a7ccd8cdc67b3fc590cce
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-10-09 19:58:23 +0300

    Create ExecutedOrder.cs

---
+++ KunaApi/DTO/Bodies/ExecutedOrder.cs
@@ -0,0 +1,19 @@
+﻿using Newtonsoft.Json;
+
+namespace KunaApi.DTO.Bodies
+{
+    public class ExecutedOrder
+    {
+        [JsonProperty("start")]
+        public long Start { get; set; }
+
+        [JsonProperty("end")]
+        public long End { get; set; }
+
+        [JsonProperty("limit")]
+        public ushort Limit { get; set; }
+
+        [JsonProperty("sort")]
+        public sbyte Sort { get; set; }
+    }
+}
