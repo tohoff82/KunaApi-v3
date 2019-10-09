3d482a3434f28a28ace4dcb822c235e87daa8bf3
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-10-09 19:59:29 +0300

    Update OrdersService.cs

--- KunaApi/Services/Implements/OrdersService.cs
+++ KunaApi/Services/Implements/OrdersService.cs
@@ -1,4 +1,5 @@
-﻿using System.Threading.Tasks;
+﻿using System;
+using System.Threading.Tasks;
 using System.Collections.Generic;
 using KunaApi.POCO.Requests;
 using KunaApi.DTO.Answers;
@@ -27,10 +28,21 @@ namespace KunaApi.Services.Implements
             return _builder.CreateOrders(crudeOrders);
         }
 
-        public async Task<IEnumerable<Order>> GetExecutedOrdersAsync(string marketMarker)
+        public async Task<IEnumerable<Order>> GetAllExecutedOrdersAsync(
+            DateTime start, DateTime end, ushort limit, sbyte sort)
         {
             string[][] crudeOrders = await HttpPostAsync<string[][]>(
-                        new ExecutedOrdersRequest(marketMarker),
+                        new ExecutedOrdersRequest(start, end, limit, sort),
+                       _options.PublicKey, _options.SecretKey);
+
+            return _builder.CreateOrders(crudeOrders);
+        }
+
+        public async Task<IEnumerable<Order>> GetExecutedOrdersAsync(string marketMarker,
+            DateTime start, DateTime end, ushort limit, sbyte sort)
+        {
+            string[][] crudeOrders = await HttpPostAsync<string[][]>(
+                        new ExecutedOrdersRequest(marketMarker, start, end, limit, sort),
                        _options.PublicKey, _options.SecretKey);
 
             return _builder.CreateOrders(crudeOrders);
