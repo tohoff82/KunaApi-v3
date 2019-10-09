049b72081f5420ed862f0f0eebd2341bdfc07d25
Татьяна Тимохова <tanyusha-dmitrenko@mail.ua>
2019-10-09 19:58:28 +0300

    Create Extensions.cs

---
+++ KunaApi/Extensions/Extensions.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using System.Text;
+
+namespace KunaApi.Extensions
+{
+    public static class Extensions
+    {
+        public static string Separate(this string[] data, string separator)
+        {
+            var sb = new StringBuilder(data.First());
+
+            if (data.Length > 1)
+            {
+                for (int i = 1; i < data.Length; i++)
+                {
+                    sb.AppendFormat("{0}{1}", separator, data[i]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
